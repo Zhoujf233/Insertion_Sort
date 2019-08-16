9797b304d6ec0ff087b5155efe85e824c9dbfe9f
zhoujf <zhoujf1636@163.com>
2019-08-16 19:06:11 +0800

    aa

--- Insertion_Sort/main.cpp
+++ Insertion_Sort/main.cpp
@@ -7,9 +7,32 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void insertion_sort(int arr[],int size)
+{
+    int i,j,temp;
+    for(i = 1; i < size; i++)
+    {
+        temp = arr[i];
+        for(j = i; j > 0 && arr[j - 1] > temp; j--)
+        {
+            arr[j] = arr[j - 1];
+        }
+        arr[j] = temp;
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    int arr[] = {22,34,3,32,82,55,89,50,37,5,64,35,9,70};
+    int size = (int) sizeof(arr)/sizeof(*arr);
+    insertion_sort(arr, size);
+    int i;
+    for(i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
