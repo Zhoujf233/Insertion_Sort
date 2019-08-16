d469ae510f3aad5a942ff6ac8c6e6ea70a19d6e9
zhoujf <zhoujf1636@163.com>
2019-08-16 19:56:30 +0800

    dd

--- Insertion_Sort/main.cpp
+++ Insertion_Sort/main.cpp
@@ -9,7 +9,8 @@
 #include <iostream>
 using namespace std;
 
-void insertion_sort(int arr[],int size)
+//插入排序
+void insertion_sort(int* arr,int size)
 {
     int i,j,temp;
     for(i = 1; i < size; i++)
@@ -25,11 +26,20 @@ void insertion_sort(int arr[],int size)
 
 int main(int argc, const char * argv[])
 {
-    int arr[] = {22,34,3,32,82,55,89,50,37,5,64,35,9,70};
+    int arr[] = {89,22,3,32,82,55,22,50,37,5,64,35,9,70};
     int size = (int) sizeof(arr)/sizeof(*arr);
-    insertion_sort(arr, size);
     int i;
-    for(i = 0; i < size; i++)
+    cout<<"初始顺序为: ";
+    for(i = 0;i < size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    
+    insertion_sort(arr, size);
+    
+    cout << "选择排序后顺序为：";
+    for(i = 0;i < size;i++)
     {
         cout<<arr[i]<<" ";
     }
