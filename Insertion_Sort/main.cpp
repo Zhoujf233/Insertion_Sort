d945878073fadf2e6293dc603029d3daa0681f5e
zhoujf <zhoujf1636@163.com>
2019-08-19 10:26:10 +0800

    666

--- Insertion_Sort/main.cpp
+++ Insertion_Sort/main.cpp
@@ -10,18 +10,70 @@
 using namespace std;
 
 //插入排序
-void insertion_sort(int* arr,int size)
+//void insertion_sort(int* arr,int size)
+//{
+//    int i,j,temp;
+//    for(i = 1; i < size; i++)
+//    {
+//        temp = arr[i];
+//        for(j = i; j > 0 && arr[j - 1] > temp; j--)
+//        {
+//            arr[j] = arr[j - 1];
+//        }
+//        arr[j] = temp;
+//    }
+//}
+
+//void insertion_sort(int* arr, int size)
+//{
+//    int i, j, k;
+//    for (i = 1; i < size; i++)
+//    {
+//        //为a[i]在前面的a[0...i-1]有序区间中找一个合适的位置
+//        for (j = i - 1; j >= 0; j--)
+//            if (arr[j] < arr[i])
+//                break;
+//        //如找到了一个合适的位置
+//        if (j != i - 1)
+//        {
+//            //将比a[i]大的数据向后移
+//            int temp = arr[i];
+//            for (k = i - 1; k > j; k--)
+//                arr[k + 1] = arr[k];
+//            //将a[i]放到正确位置上
+//            arr[k + 1] = temp;
+//        }
+//    }
+//}
+
+//优化1:将搜索和数据后移这二个步骤合并
+//void insertion_sort(int* arr, int size)
+//{
+//    int i, j;
+//    for (i = 1; i < size; i++)
+//        if (arr[i] < arr[i - 1])
+//        {
+//            int temp = arr[i];
+//            for (j = i - 1; j >= 0 && arr[j] > temp; j--)
+//                arr[j + 1] = arr[j];
+//            arr[j + 1] = temp;
+//        }
+//}
+
+//优化2:用数据交换代替数据后移
+void swap(int *a, int *b)//交换两数
 {
-    int i,j,temp;
-    for(i = 1; i < size; i++)
-    {
-        temp = arr[i];
-        for(j = i; j > 0 && arr[j - 1] > temp; j--)
-        {
-            arr[j] = arr[j - 1];
-        }
-        arr[j] = temp;
-    }
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void insertion_sort(int* arr, int size)
+{
+    int i, j;
+    for (i = 1; i < size; i++)
+        for (j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--)
+            swap(arr[j], arr[j + 1]);
 }
 
 int main(int argc, const char * argv[])
